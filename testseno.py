fad0b9154ab619ed7beafc1889c1cc268e4e9522
ale-mendez <alemendez@iafe.uba.ar>
2019-03-19 16:38:17 -0300

    ploteo de gaussiana y senos

--- testseno.py
+++ testseno.py
@@ -9,9 +9,13 @@ def fsin(x,omega):
     y = np.sin(x*omega)
     return y
 
+def Gauss(x, Sigma, A, X0):
+    y = A*np.exp(-((x-X0)/(2*Sigma))**2)
+    return y
+
 # y = fsin(x,1.0)
 
 plt.plot(x,fsin(x,1.0))
 plt.plot(x,fsin(x,2.0))
-plt.plot(x,fsin(x,3.0))
+plt.plot(x,Gauss(x,1,1,3))
 plt.show()
